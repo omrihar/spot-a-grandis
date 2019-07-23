3e6fe2f959604bae1c025696f02931efd06d6a24
Omri Har-Shemesh <omrihar@gmail.com>
2019-07-23 22:36:43 +0400

    made sidebar relevant

--- src/src/layouts/MyLayout.vue
+++ src/src/layouts/MyLayout.vue
@@ -1,81 +1,31 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
+<template lang="pug">
+  q-layout(view="lHh Lpr lFf")
+    q-drawer(v-model="leftDrawerOpen" bordered content-class="bg-grey-2")
+      q-list
+        q-item-label(header) Menu
 
-    <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://github.com/quasarframework/"
-        >
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://chat.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://forum.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://twitter.com/quasarframework"
-        >
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+        q-item(clickable tag="a" to="/")
+          q-item-section(avatar)
+            q-icon( name="home")
+          q-item-section
+            q-item-label Home
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        q-item(clickable tag="a" to="/report")
+          q-item-section(avatar)
+            q-icon( name="send")
+          q-item-section
+            q-item-label Report
+            q-item-label(caption) Report a new sighting<
+
+        q-item(clickable tag="a" to="/map")
+          q-item-section(avatar)
+            q-icon(name="map")
+          q-item-section
+            q-item-label View Map
+            q-item-label(caption) See map with known sightings
+
+    q-page-container
+      router-view
 </template>
 
 <script>
