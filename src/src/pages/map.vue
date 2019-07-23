86a273866abab34cd305b6ac0ab7ca5b6da7322d
Omri Har-Shemesh <omrihar@gmail.com>
2019-07-23 23:02:43 +0400

    add attribution to gecko icon

--- src/src/pages/map.vue
+++ src/src/pages/map.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   q-page
     l-map(:center="center", :zoom="zoom" id="mymap" @update:center="c => center = c" @update:zoom="z => zoom = z")
-      l-tile-layer(:url="url")
+      l-tile-layer(:url="url" :attribution="attribution")
       l-marker(:lat-lng="center" :icon="icon")
 </template>
 <script>
@@ -16,6 +16,7 @@ export default {
       center: [-20.250279813039555 , 57.674102783203125],
       zoom: 11,
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: 'Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>',
       icon: L.icon({
         iconUrl: 'assets/images/gecko.svg',
         iconSize: [32, 37],
